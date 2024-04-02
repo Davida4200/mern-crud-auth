46de66c6bc7a6c14e2b7b87fc5718b2b2d96b8d1
Davida4200 <ekhizde99@gmail.com>
2024-04-01 23:24:03 -0500

    Added

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://davida4200:panacea@cluster0.jnl9m2o.mongodb.net/mern-auth')
+    await mongoose.connect('database_url')
     console.log('Database connected')
   } catch (error) {
     console.log(error)
